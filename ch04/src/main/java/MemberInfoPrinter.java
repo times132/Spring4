3c3c5a192eaefc46db6673fe354817929c5d4199
times132 <times132@gmail.com>
2019-03-16 19:56:32 +0900

    ch05 javaconfig-1

---
+++ ch04/src/main/java/MemberInfoPrinter.java
@@ -0,0 +1,22 @@
+public class MemberInfoPrinter {
+    private MemberDao memberDao;
+    private MemberPrinter printer;
+
+    public void setMemberDao(MemberDao memberDao){
+        this.memberDao = memberDao;
+    }
+
+    public void setPrinter(MemberPrinter printer){
+        this.printer = printer;
+    }
+
+    public void printMemberInfo(String email){
+        Member member = memberDao.selectByEmail(email);
+        if (member == null){
+            System.out.println("데이터없음\n");
+            return;
+        }
+        printer.print(member);
+        System.out.println();
+    }
+}
---
+++ ch05/src/main/java/MemberInfoPrinter.java
@@ -0,0 +1,32 @@
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberInfoPrinter {
+    @Autowired
+    private MemberDao memberDao;
+    private MemberPrinter printer;
+
+    public void setMemberDao(MemberDao memberDao){
+        this.memberDao = memberDao;
+    }
+
+    @Autowired
+    public void setPrinter(MemberPrinter printer){
+        this.printer = printer;
+    }
+
+    public void printMemberInfo(String email){
+        Member member = memberDao.selectByEmail(email);
+        if (member == null){
+            System.out.println("데이터없음\n");
+            return;
+        }
+        printer.print(member);
+        System.out.println();
+    }
+
+    @Autowired
+    public void injectDependency(MemberDao memberDao, MemberPrinter printer){
+        this.memberDao = memberDao;
+        this.printer = printer;
+    }
+}
